2d35c583292775b2c05a1b17e538b80abb9ddb98
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2016-05-12 23:43:31 +0300

    Factorial Compiled Correctly

---
+++ Factorial/Factorial.pas
@@ -0,0 +1,19 @@
+﻿{Author: Constantinos Georgiou}
+Program factorial;
+Var
+Num:Integer;
+Function factorial(N:Integer):Integer;
+	Var
+	i,multiplier:Integer;
+	Begin
+	multiplier:=1;
+	For i:=1 to N Do
+		multiplier:=multiplier*i;
+	factorial:=multiplier;
+	End;
+Begin
+Write('It would be nice if you typed a number: ');
+Readln(Num);
+Writeln('The factorial of ',Num,' is ',factorial(Num):5);
+Readln();
+End.
